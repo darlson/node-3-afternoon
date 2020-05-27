7de83c2e508ce89608f495b9e5305a3791b98fe5
darlson <davertiser@gmail.com>
2020-05-27 16:09:45 -0600

    updated files

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const express = require('express')
 const massive = require('massive')
+const ctrl = require('./products_controller')
+
 const app = express()
 const { SERVER_PORT, CONNECTION_STRING } = process.env
 
@@ -12,6 +14,12 @@ massive({
 }).then(dbInstance => {
     app.set('db', dbInstance)
 }).catch(err => console.log(err))
+
+app.post('/api/products', ctrl.create)
+app.get('/api/products', ctrl.getAll)
+app.get('/api/products/:id', ctrl.getOne)
+app.put('/api/products/:id', ctrl.update)
+app.delete('/api/products/:id', ctrl.delete)
 
 
 
---
+++ server/products_controller.js
@@ -0,0 +1,83 @@
+module.exports = {
+  create: (req, res, next) => {
+    const dbInstance = req.app.get("db");
+    const { name, description, price, image_url } = req.body;
+
+    dbInstance
+      .create_product([name, description, price, image_url])
+      .then(() => res.sendStatus(200))
+      .catch((err) => {
+        res
+          .status(500)
+          .send({
+            errorMessage:
+              "Whoopsy daisy, something went wrong. We'll get it fixed.",
+          });
+        console.log(err);
+      });
+  },
+  getOne: (req, res, next) => {
+    const dbInstance = req.app.get("db");
+
+    dbInstance
+      .read_product(id)
+      .then((product) => res.status(200).send(product))
+      .catch((err) => {
+        res
+          .status(500)
+          .send({
+            errorMessage:
+              "Whoopsy daisy, something went wrong. We'll get it fixed.",
+          });
+        console.log(err);
+      });
+  },
+  getAll: (req, res, next) => {
+    const dbInstance = req.app.get("db");
+
+    dbInstance
+      .read_products()
+      .then((products) => res.status(200).send(products))
+      .catch((err) => {
+        res
+          .status(500)
+          .send({
+            errorMessage:
+              "Whoopsy daisy, something went wrong. We'll get it fixed.",
+          });
+        console.log(err);
+      });
+  },
+  update: (req, res, next) => {
+    const dbInstance = req.app.get("db");
+
+    dbInstance
+      .update_product([params.id, query.desc])
+      .then(() => res.sendStatus(200))
+      .catch((err) => {
+        res
+          .status(500)
+          .send({
+            errorMessage:
+              "Whoopsy daisy, something went wrong. We'll get it fixed.",
+          });
+        console.log(err);
+      });
+  },
+  delete: (req, res, next) => {
+    const dbInstance = req.app.get("db");
+
+    dbInstance
+      .delete_product(id)
+      .then(() => res.sendStatus(200))
+      .catch((err) => {
+        res
+          .status(500)
+          .send({
+            errorMessage:
+              "Whoopsy daisy, something went wrong. We'll get it fixed.",
+          });
+        console.log(err);
+      });
+  },
+};
